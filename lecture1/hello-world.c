d6cb22510f692576c0d612e6282f3aad8877807d
Michael Hendricks <mihendricks1@gmail.com>
2022-05-13 08:19:55 +0200

    add hi

--- lecture1/hello-world.c
+++ lecture1/hello-world.c
@@ -1,13 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void hi(void);
+
 int main(void)
 {
     string name = get_string("What is you CS50 name?"); 
     // Greeting someone
     char names[10];
+    hi();
     printf("What is your name?");
      scanf("%[^\n]%*c", names);
     printf("hello, %s\n", names);
     printf("hello, cs50 %s\n", name);
+}
+
+void hi(void)
+{
+    printf("hi");
 }
